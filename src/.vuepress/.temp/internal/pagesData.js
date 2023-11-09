6335f4e334a3afa2b7285a7d179a0e51e63094ea
Mr.Hope <mister-hope@outlook.com>
2023-11-09 19:56:29 +0800

    feat: init reproduction

---
+++ src/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,6 @@
+export const pagesData = {
+  // path: /
+  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"/home/mister-hope/projects/community-docs/test/src/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/home/mister-hope/projects/community-docs/test/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+}
---
+++ src/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,4 @@
+<template><div><h1 id="content" tabindex="-1"><a class="header-anchor" href="#content" aria-hidden="true">#</a> Content</h1>
+</div></template>
+
+
